bccc04a2f2ce7f06763ddab20343383f15b7f72e
Daniel <118110415+FlawlessDaniel31@users.noreply.github.com>
2023-11-08 07:18:36 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD_2023
+{
+    public class Car
+    {
+       
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string modelName, string colorName, int yearValue, int maxSpeedValue )
+        {
+            name = modelName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+        public void fullThrotle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XD_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustan", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 100);
+            Car Volvo = new Car("XC-96");
+            Car BMW = new Car { name = "da", color = "red", year = 1000, maxSpeed = 50 };
+
+            /*
+            Car1 Ford = new Car1(); 
+            Car1 Opel = new Car1();
+            Ford.name = "mustang";
+            Ford.year = 2021;
+            Ford.color = "white";
+            Ford.maxSpeed = 100;
+            Opel.name = "astra";
+            Opel.year = 2020;
+            Opel.color = "blue";
+            Opel.maxSpeed = 120;
+            */
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrotle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.maxSpeed);
+            Opel.fullThrotle();
+            Console.ReadKey();
+        }
+    }
+}
